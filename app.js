b239aacd93ab0b4f48d3cff4e55bb8708655ec6a
melo-velo <JAAK8280@STTHOMAS.EDU>
2022-11-11 14:57:29 -0600

    Initial commits

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=> {
+    res.render('index', {title: 'final project homepage'});
+});
+
+app.listen(PORT, ()=> {
+    console.log('Listening on port '+ PORT);
+});
+
