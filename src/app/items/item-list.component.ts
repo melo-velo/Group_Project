3f02bed47770c229511e8757c3ef1041fa45f2e8
melo-velo <JAAK8280@STTHOMAS.EDU>
2022-11-23 11:17:13 -0600

    Added basic data layout
    Correctly renders on the screen

--- src/app/items/item-list.component.ts
+++ src/app/items/item-list.component.ts
@@ -5,5 +5,30 @@ import { Component } from "@angular/core";
   templateUrl: './item-list.component.html'
 })
 export class ItemListComponent {
+  pageHeader: string = 'Item List';
+  imgWidth = 50;
+  imgMargin = 2;
 
+  highValueItems: any[] = [
+    {
+      "imageUrl": "assets/images/Martin.jpg",
+      "productName": "Guitar",
+      "productId": "567-797372-IBN",
+      "purchasePrice": 400,
+      "purchaseLocation": "Guitar Center",
+      "datePurchased": "May 5, 2018",
+      "condition": "Mint",
+      "category": "Instruments"
+    },
+    {
+      "imageUrl": "assets/images/bike.png",
+      "productName": "Bike",
+      "productId": "590-7943372-COL",
+      "purchasePrice": 2000,
+      "purchaseLocation": "Angry Catfish",
+      "datePurchased": "March 7, 2009",
+      "condition": "Used",
+      "category": "Sports Equipment"
+    }
+  ];
 }
