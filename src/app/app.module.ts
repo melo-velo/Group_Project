06dfe94ce6754d63200eb79fa9954ba2bc78f216
melo-velo <JAAK8280@STTHOMAS.EDU>
2022-12-04 11:58:12 -0600

    File cleanup and organization
    Spelling changes, removed unused import statements, and organized view components into their own folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import {MatCardModule} from '@angular/material/card';
-import { ViewLocationComponent } from './view-location/view-location.component';
-import { ViewHeaderComponent } from './view-header/view-header.component';
-import { ViewSidebarComponent } from './view-sidebar/view-sidebar.component';
-import { ViewLocationCardComponent } from './view-location-card/view-location-card.component';
-import { ViewHomeComponent } from './view-home/view-home.component';
+import { ViewLocationComponent } from './view/view-location/view-location.component';
+import { ViewHeaderComponent } from './view/view-header/view-header.component';
+import { ViewSidebarComponent } from './view/view-sidebar/view-sidebar.component';
+import { ViewLocationCardComponent } from './view/view-location-card/view-location-card.component';
+import { ViewHomeComponent } from './view/view-home/view-home.component';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
-import { ViewLocationFormComponent } from './view-location-form/view-location-form.component';
+import { ViewLocationFormComponent } from './view/view-location-form/view-location-form.component';
 import {MatInputModule} from '@angular/material/input';
-import { ViewItemComponent } from './view-item/view-item.component';
+import { ViewItemComponent } from './view/view-item/view-item.component';
 import {MatTableModule} from '@angular/material/table';
-import { ViewListFormComponent } from './view-list-form/view-list-form.component';
+import { ViewListFormComponent } from './view/view-list-form/view-list-form.component';
 
 
 
