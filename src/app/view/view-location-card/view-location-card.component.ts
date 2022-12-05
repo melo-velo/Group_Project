de3d04a322d7ddd7dc5362997578432928c7b56f
Guy Lawrence <lawr0015@stthomas.edu>
2022-12-04 21:26:51 -0600

    Read local JSON file, staging for Node backend

--- src/app/view/view-location-card/view-location-card.component.ts
+++ src/app/view/view-location-card/view-location-card.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IList } from "../../items/item";
 
 @Component({
   selector: 'pm-view-location-card',
   templateUrl: './view-location-card.component.html',
   styleUrls: ['./view-location-card.component.scss']
 })
-export class ViewLocationCardComponent {
-  public locationName: string = 'Location Name';
-  public address: string = 'Address';
+export class ViewLocationCardComponent implements OnInit{
+  // Bring the IList 
+  @Input() item? : IList;
+
+  public locationName: string = '';
+  public address: string = '';
+
+  ngOnInit(){
+    console.log("view-location-card NgOnInit()");
+    if (this.item != undefined)
+    {
+
+      
+      this.locationName = this.item.listname;
+      this.address = this.item.listaddress;  
+    }
+  }
 }
