4d258b4ae440ba169b719704c13dc3693f2fc05b
dang1840 <dang1840@stthomas.edu>
2022-12-08 13:34:05 -0600

    Updated UI

---
+++ src/app/page-inventory/page-inventory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-page-inventory',
+  templateUrl: './page-inventory.component.html',
+  styleUrls: ['./page-inventory.component.scss']
+})
+export class PageInventoryComponent {
+
+}
