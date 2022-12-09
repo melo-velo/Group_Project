87260cee38202379c33a885cbeab92336f3b15cb
melo-velo <JAAK8280@STTHOMAS.EDU>
2022-12-09 13:59:08 -0600

    Added about screen template and modules
    Includes detailed example of my own info with TODO details for the rest of the team members to fill in

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+export interface mailtoAsset {
+  headline: string;
+  subtexts: string[];
+}
 
 @Component({
   selector: 'pm-about',
@@ -6,5 +12,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  pageHeader: string = 'Item List';
+  imgWidth = 150;
+  imgMargin = 6;
+  mailtoHeader = "mailto:?";
+  emailSubject = "subject=";
+  emailBody = "body=";
+
+  emailTextStrings = 'mailto:?subject=Email to company;body=Some sort of idea here.'
+
+  subjectLine = "Subject line here for email"
+
+  assets: mailtoAsset[] = [
+    {
+      headline: "Email to Itemize",
+      subtexts: ["My email body here "]
+    }
+  ];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getEmailStrings(assets: mailtoAsset[], subject: string = "Email to Itemize company") {
+      let myString = "";
+      assets.forEach(asset => {
+        myString = myString + asset.headline.toUpperCase() + asset.subtexts});
+      const url = `${this.mailtoHeader}${this.emailSubject}${subject}&${this.emailBody}${myString}`;
+      return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 
 }
